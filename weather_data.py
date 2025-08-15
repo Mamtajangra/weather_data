b0cd57ceac081a335c7c65db8a4bc3fa534092b8
Mamta Jangra <mamtajangra204@gmail.com>
2025-08-15 20:35:12 +0530

    Update city variable in weather data script to Fresno

--- weather_data.py
+++ weather_data.py
@@ -2,15 +2,27 @@ import requests
 import os
 import  dotenv
 
-
 dotenv.load_dotenv()
 key = os.getenv("API")
-city = "Panipat"
+
+city = "Fresno"
 weather_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&units=metric"
 response = requests.get(weather_url)
 data = response.json()
-#
+
 print(data)
+
+      
+
+
+
+
+
+
+
+
+
+
 
 
 
